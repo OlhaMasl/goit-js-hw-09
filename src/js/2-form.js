e0d8501bb9bc7eb279d5f477e380f943c682c257
OlhaMasl <maslakok@gmail.com>
2024-05-13 21:04:05 +0300

    HW09-version01

---
+++ src/js/2-form.js
@@ -0,0 +1,50 @@
+const formData = {
+    email: "",
+    message: "",
+};
+
+const feedback = "feedback-form-state";
+
+const feedbackForm = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+
+getStorageData();
+
+function onFormInput(event) {
+    event.preventDefault();
+    // formData[event.target.name] = event.target.value;
+    formData.email = emailInput.value.trim();
+    formData.message = textarea.value.trim();
+
+    const jsonFormData = JSON.stringify(formData);
+    // console.log(jsonFormData);
+    localStorage.setItem(feedback, jsonFormData);
+
+}
+
+// console.log(_.throttle);
+
+feedbackForm.addEventListener("input", _.throttle(onFormInput, 1000));
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (emailInput.value === "" || textarea.value === "") {
+        alert("Fill please all fields"); 
+    } else {
+        console.log(formData);
+    };
+    localStorage.removeItem(feedback);
+    event.currentTarget.reset();
+};
+
+feedbackForm.addEventListener("submit", onFormSubmit)
+
+function getStorageData() {
+    const storageData = localStorage.getItem(feedback);
+    if (storageData) {
+        const objStorageData = JSON.parse(storageData);
+        emailInput.value = objStorageData.email;
+        textarea.value = objStorageData.message;
+    };
+};
